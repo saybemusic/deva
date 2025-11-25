6892d4efc4176af99a593bd97ff78af0b0d3f52a
Antoine Ricaud <antoine.ricaud24@gmail.com>
2025-11-25 11:48:16 +0100

    root vers sign in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :configure_permitted_parameters, only: :home
 
   def home
   end
+
+  def landing
+    if user_signed_in?
+      redirect_to home_path
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+
 end
