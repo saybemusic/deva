03ce2ce10d207feecea3174a40aec51c522a454d
Sebastien Richard <sebastienrlr@gmail.com>
2025-11-28 15:16:37 +0100

    Push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_28_094135) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_28_113930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_28_094135) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "exercise_id"
+    t.string "context"
     t.index ["exercise_id"], name: "index_chats_on_exercise_id"
     t.index ["user_id"], name: "index_chats_on_user_id"
   end
